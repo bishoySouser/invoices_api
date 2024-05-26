3e016ee3439dcd9890565e9508ae82a249c688fe
Bishoysouser <Bishoysouser@gmail.com>
2024-05-26 23:30:39 +0300

    customer Query

---
+++ app/Services/V1/CustomerQuery.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\V1;
+
+use Illuminate\http\Request;
+
+class CustomerQuery
+{
+    protected $safaParms = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt']
+    ];
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>='
+    ];
+
+    public function transform(Request $request)
+    {
+        $eloQuery = [];
+
+        foreach ($this->safaParms as $parm => $operators) {
+            $query = $request->query($parm);
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$parm] ?? $parm;
+
+            foreach ($operators as $operator) {
+                if(isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+}
